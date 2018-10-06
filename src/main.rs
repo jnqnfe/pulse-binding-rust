2428832ef76e5a52b55c48e1f11a2dad09d389ed
Lyndon Brown <jnqnfe@gmail.com>
2018-10-06 03:00:23 +0100

    workspace: simplify

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-//! Dummy bin crate
-//!
-//! Acts as a workspace, and groups all crates together (as dependencies) for building in one go.
-
-#![doc(html_logo_url = "https://github.com/jnqnfe/pulse-binding-rust/raw/master/logo.png",
-       html_favicon_url = "https://github.com/jnqnfe/pulse-binding-rust/raw/master/favicon.ico")]
-#![doc(html_no_source)]
-
-fn main() {}
