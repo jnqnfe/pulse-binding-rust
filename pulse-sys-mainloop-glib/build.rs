96fc8e722995b3cd8c94c5dab7412a9bf269fb77
Daniel Schaefer <git@danielschaefer.me>
2018-10-22 22:42:01 +0200

    Use pkg-config to determine linker options
    jnqnfe: Tweaked slightly from the original revised patch cherry-picked from
    the github submission [1]:
     - Bumped the minimum PA version down to 10.x
     - Reverted removal of ``#[link(name="pulse")]`` attribute on extern C
       blocks, which it won't compile without
     - Tweaked probed lib names
    
    [1]: https://github.com/jnqnfe/pulse-binding-rust/pull/9
    
    Signed-off-by: Lyndon Brown <jnqnfe@gmail.com>

--- pulse-sys-mainloop-glib/build.rs
+++ pulse-sys-mainloop-glib/build.rs
@@ -1,8 +1,12 @@
+#[cfg(target_os="linux")]
+extern crate pkg_config;
+
+#[cfg(target_os="linux")]
 fn main() {
-    if cfg!(target_os="linux") {
-        println!("cargo:rustc-link-lib=pulse-mainloop-glib::libpulse-mainloop-glib.so.0");
-    }
-    else {
-        println!("cargo:rustc-link-lib=pulse-mainloop-glib");
-    }
+    pkg_config::Config::new().atleast_version("10.0").probe("libpulse-mainloop-glib").unwrap();
+}
+
+#[cfg(not(target_os="linux"))]
+fn main() {
+    println!("cargo:rustc-link-lib=pulse-mainloop-glib");
 }
