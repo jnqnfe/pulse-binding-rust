59d1baa6c4282d6fcb4414ae2ace9c8e4463e93e
Lyndon Brown <jnqnfe@gmail.com>
2019-08-17 04:13:45 +0100

    workspace: re-simplify
    so apparently control of member features from workspaces just isn't
    supported yet. so let's revert the `[package]` based workspace setup that
    i had thought might work.
    
    note, with the `[package]` model, `cargo [build|check|doc]` commands worked
    as wanted as long as you did **not** use `--all`, which enabled default
    member crate features if used, breaking feature control. use of `cargo test`
    did not work since it only would run tests for the workspace lib, not those
    for members.

--- workspace/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Workspace crate
-//!
-//! Groups all crates together (as dependencies) for building and testing in one go.
-
-#![doc(html_logo_url = "https://github.com/jnqnfe/pulse-binding-rust/raw/master/logo.png",
-       html_favicon_url = "https://github.com/jnqnfe/pulse-binding-rust/raw/master/favicon.ico")]
-#![doc(html_no_source)]
