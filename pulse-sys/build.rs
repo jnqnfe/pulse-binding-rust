fac498e3467265cf60a5908ca76c374b7736a89d
Lyndon Brown <jnqnfe@gmail.com>
2019-08-17 23:44:31 +0100

    build: prep for more versions

--- pulse-sys/build.rs
+++ pulse-sys/build.rs
@@ -3,9 +3,11 @@ extern crate pkg_config;
 
 #[cfg(target_os="linux")]
 fn main() {
-    let min_version = match cfg!(feature="pa_v12_compatibility") {
-        true => "12.0",
-        false => "8.0",
+    let min_version = {
+        #[cfg(feature="pa_v12_compatibility")]
+        { "12.0" }
+        #[cfg(not(feature="pa_v12_compatibility"))]
+        { "8.0" }
     };
     // Try package-config first
     let pc = pkg_config::Config::new().atleast_version(min_version).probe("libpulse");
