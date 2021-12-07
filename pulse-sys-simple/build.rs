a9b4cd6c5f3b6e35f8f8ce06234c1d72396b597b
Thibaut Lorrain <fredszaq@gmail.com>
2021-12-07 16:57:57 +0100

    use ProbeError from pkg-config
    pkgconfig 0.3.23 changed added a new error in the case of a probe
    failure (see https://github.com/rust-lang/pkg-config-rs/pull/127)
    
    This can cause previously building configuration (ie confs without the
    dev package installed for pulseaudio) to fail.
    
    This change uses the new error from pkgconfig instead of the old one.

--- pulse-sys-simple/build.rs
+++ pulse-sys-simple/build.rs
@@ -26,7 +26,7 @@ fn main() {
     config.cargo_metadata(false);
     let fallback = match config.probe(lib_name) {
         // We assume all failure here (being a non-version specific check) indicates no *.pc file
-        Err(pkg_config::Error::Failure { .. }) => {
+        Err(pkg_config::Error::ProbeFailure { .. }) => {
             println!("cargo:warning=Pkg-config seems to not know about PulseAudio (dev package not installed?), \
                        trying generic fallback...");
             true
